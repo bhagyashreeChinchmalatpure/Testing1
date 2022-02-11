41d6c6ee68ecbdb65f14818acc631e8f923c9e29
lenovo <lenovo@DESKTOP-UQBQ9M4>
2022-02-11 21:50:11 +0530

    by shree1

--- GitDemo1/src/Demo.java
+++ GitDemo1/src/Demo.java
@@ -1,9 +1,17 @@
 
 public class Demo {
+	
+	
+	public void m1()
+	{
+		
+		System.out.println("hello");
+	}
 	public static void main(String[] args) {
 		
 		
 		System.out.println("hello main of git application");
+		
 	}
 
 }
